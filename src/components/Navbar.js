f264f19d09c1a09035ae7481add5c686439b7732
Aebel Shajan <aebelshajan@gmail.com>
2024-03-24 21:09:40 +0000

    Create boilerplate using npx create-react-app

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css'
+const Navbar = () => {
+    return (<header>
+        <h1>ART GALLERY</h1>
+        <nav>
+            <button>
+                Gallery
+            </button>
+            <button>
+                Doodle
+            </button>
+        </nav>
+    </header>);
+}
+ 
+export default Navbar;
